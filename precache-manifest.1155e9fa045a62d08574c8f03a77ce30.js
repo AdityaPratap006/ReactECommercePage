2016aa5be162e4f2f2a7d989129b3340b628cfe9
Aditya Pratap <adityapratap7344@gmail.com>
2019-06-02 18:47:21 +0530

    Updates

---
+++ precache-manifest.1155e9fa045a62d08574c8f03a77ce30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54932d2819aabee3305166c5c9461bbd",
+    "url": "/ReactECommercePage/index.html"
+  },
+  {
+    "revision": "196ff842dbee920749f1",
+    "url": "/ReactECommercePage/static/css/2.e4bf0393.chunk.css"
+  },
+  {
+    "revision": "9e799a3c5129f5d10f3a",
+    "url": "/ReactECommercePage/static/css/main.faaa5ae5.chunk.css"
+  },
+  {
+    "revision": "196ff842dbee920749f1",
+    "url": "/ReactECommercePage/static/js/2.f2a264dc.chunk.js"
+  },
+  {
+    "revision": "9e799a3c5129f5d10f3a",
+    "url": "/ReactECommercePage/static/js/main.43e79f00.chunk.js"
+  },
+  {
+    "revision": "83129633f9c8f5e41fba",
+    "url": "/ReactECommercePage/static/js/runtime~main.408c2ba7.js"
+  },
+  {
+    "revision": "11d671a146318bb61c4c60d2c40f56aa",
+    "url": "/ReactECommercePage/static/media/man-user.11d671a1.svg"
+  },
+  {
+    "revision": "f5d771e7852bf3743697f1a4b11be38e",
+    "url": "/ReactECommercePage/static/media/shopping-cart.f5d771e7.svg"
+  }
+]);
